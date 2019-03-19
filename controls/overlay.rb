11e5d2badf484b10f3815bbe343a3834cfd34e4d
Haynes Jr <dhaynes@mitre.org>
2019-03-19 14:09:29 -0400

    adding draft overlay

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,7 +1,57 @@
 # encoding: utf-8
 
 include_controls 'cis-docker-ce-profile' do
+  control 'M-3.18' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
 
+  control 'M-3.2' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end 
+
+  control 'M-3.6' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+
+  control 'M-3.12' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+
+  control 'M-3.16' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+
+  control 'M-3.8' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+
+  control 'M-5.2' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+  
+  control 'M-3.14' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+
+  control 'M-3.2' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+
+  control 'M-3.1' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+
+  control 'M-5.1' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
+
+  control 'M-1.3' do
+
+  end
+  
+  control 'M-3.4' do
+    tag "nist": ['AC-3', 'Rev_4']
+  end
 end
 
 
